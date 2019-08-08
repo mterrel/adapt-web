272b19f6ca092fa231b404bdc9afcc0b8727f5e9
Mark Terrel <mark@unbounded.systems>
2019-08-08 15:14:12 -0600

    Add Makefile and script for automating commits

---
+++ Makefile
@@ -0,0 +1,69 @@
+all: test
+.PHONY: all
+
+NODE_SUBDIRS := adapt-web-components website
+PKG_JSONS := package.json $(addsuffix /package.json,$(NODE_SUBDIRS))
+
+# Don't depend on the timestamp of the actual node_modules directory.
+# Depend on the timestamp of a .install-success file that says when
+# yarn install actually last successfully completed
+node_modules/.install-success: yarn.lock $(PKG_JSONS)
+	yarn
+	touch $@
+
+AWC_SRC_FILES:=$(shell find adapt-web-components \( -path adapt-web-components/dist -o -path adapt-web-components/node_modules -o -path adapt-web-components/.rpt2_cache \) -prune -o -type f -print)
+
+node_modules/.awc-build-success: $(AWC_SRC_FILES)
+	cd adapt-web-components && yarn run build
+	touch $@
+
+awc-build: node_modules/.awc-build-success
+.PHONY: awc-build
+
+DOCS_SRC_FILES:=$(shell find docs/ -type f)
+WEBSITE_SRC_FILES:=$(shell find website \( -path website/build -o -path website/node_modules \) -prune -o -type f -print)
+
+node_modules/.website-build-success: $(WEBSITE_SRC_FILES) $(DOCS_SRC_FILES) node_modules/.awc-build-success
+	cd website && yarn run build
+	touch $@
+
+website-build: node_modules/.website-build-success
+.PHONY: website-build
+
+build: awc-build website-build
+.PHONY: build
+
+test: build
+.PHONY: test
+
+ADAPT_DOCS_UPDATED:=$(addsuffix /dist/.docs_success,$(PROJ_DIRS_ABS))
+ADAPT_DOCS_FILES:=$(shell find $(REPO_ROOT)/docs/ -type f)
+
+node_modules/.update-from-adapt-success: $(ADAPT_DOCS_FILES) $(ADAPT_DOCS_UPDATED)
+	@if [ -z "$(REPO_ROOT)" ]; then \
+		echo Error: REPO_ROOT not set in web Makefile. ; \
+		false ; \
+	fi
+	rm -rf docs/
+	mkdir docs
+	cp -R $(REPO_ROOT)/*/build/docs/* docs/
+	cp -R $(REPO_ROOT)/docs/* docs/
+	touch $@
+
+update-from-adapt: node_modules/.update-from-adapt-success
+.PHONY: update-from-adapt
+
+push-docs-master: test
+	@if [ -z "$(REPO_ROOT)" ]; then \
+		echo Error: REPO_ROOT not set in web Makefile. ; \
+		false ; \
+	fi
+	./push-docs.sh $(REPO_ROOT) master
+.PHONY: push-docs-master
+
+start-docker: node_modules/.install-success
+	printf "\n\nRunning Docusaurus server on http://localhost:3000\n\n"
+	docker run --rm -it -p35729:35729 -p3000:3000 --name docs -v $$(pwd):/app -w /app unboundedsystems/node-testimg:v2.0.1 yarn start
+.PHONY: start-docker
+
+
---
+++ push-docs.sh
@@ -0,0 +1,129 @@
+#!/usr/bin/env bash -e
+
+function usage {
+    cat <<USAGE
+
+Commits and pushes changes to the docs directory.
+
+
+Usage:
+  $0 <ADAPT_REPO> <BRANCHNAME>
+
+  ADAPT_REPO:   Path to the Adapt repo from which to pull the latest docs
+  BRANCHNAME:   Branch in adapt-web to commit/push to
+
+  FLAGS:
+      -h | --help   Display help for command
+
+Example:
+  $0 .. master
+
+USAGE
+}
+
+function error {
+    printf "\n\033[01;31mERROR: $*\033[m\n\n" >&2
+}
+
+function run {
+    printf "\n$*\n"
+    $@
+}
+
+
+if [[ $(pwd) != $(git rev-parse --show-toplevel) ]]; then
+    error MUST be run from the root of the adapt-web repo.
+    usage
+    exit 1
+fi
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+
+        -*)
+            error Unrecognized flag: $1
+            usage
+            exit 1
+            ;;
+
+        *)
+            if [[ -z $ADAPT_REPO ]]; then
+                ADAPT_REPO="$1"
+            elif [[ -z $BRANCHNAME ]]; then
+                BRANCHNAME="$1"
+            else
+                error Too many arguments
+                usage
+                exit 1
+            fi
+            ;;
+    esac
+    shift
+done
+
+if [[ -z $ADAPT_REPO || -z $BRANCHNAME ]]; then
+    error Not enough arguments
+    usage
+    exit 1
+fi
+
+if [[ -z $(git config --get user.name) ]]; then
+    if [[ -z ${GIT_USER_NAME} ]]; then
+        error Cannot determine git user.name. Set via GIT_USER_NAME environment var.
+        exit 1
+    fi
+    run git config --global user.name "${GIT_USER_NAME}"
+fi
+
+if [[ -z $(git config --get user.email) ]]; then
+    if [[ -z ${GIT_USER_EMAIL} ]]; then
+        error Cannot determine git user.email. Set via GIT_USER_EMAIL environment var.
+        exit 1
+    fi
+    run git config --global user.email "${GIT_USER_EMAIL}"
+fi
+
+if [[ -n $(git -C ${ADAPT_REPO} status --porcelain) ]]; then
+    error Cannot commit adapt-web changes when adapt has uncommitted changes
+    git -C ${ADAPT_REPO} status
+    exit 1
+fi
+
+ADAPT_COMMIT=$(git -C ${ADAPT_REPO} log -1 --oneline --decorate=short)
+
+# Ensure we're up to date with latest
+run git checkout ${BRANCHNAME}
+run git pull --ff-only
+
+# Remove any staged changes
+run git reset
+
+# Add only the changes in the docs directory
+run git add docs/
+
+# Check for changes outside the docs directory
+UNSTAGED=$(git status --porcelain | grep -v '^. ')
+if [[ -n $UNSTAGED ]]; then
+    error Cannot commit adapt-web with changes outside the docs directory
+    run git status
+    exit 1
+fi
+
+# Check for any changes to commit
+STAGED=$(git status --porcelain | grep '^. ')
+if [[ -z ${STAGED} ]]; then
+    printf "\nNo changes to commit\n"
+    exit 0
+fi
+
+printf "\n\nCommitting the following changes to adapt-web branch ${BRANCHNAME}\n\n"
+git status
+echo
+
+run git commit -F - <<< "[auto] Updating to adapt commit: ${ADAPT_COMMIT}"
+run git push origin ${BRANCHNAME}
+
