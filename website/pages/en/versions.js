475e64d424a455b33c0dc7b35b777883b18d1845
Mark Terrel <mark@unbounded.systems>
2019-08-19 16:02:30 -0600

    Automatically generate content for /versions page Use versions.json to generate previous versions.

--- website/pages/en/versions.js
+++ website/pages/en/versions.js
@@ -16,9 +16,10 @@ const CWD = process.cwd();
 const versions = require(`${CWD}/versions.json`);
 
 function Versions(props) {
-  const {config: siteConfig} = props;
+  const {config } = props;
   const latestVersion = versions[0];
-  const { repoUrl } = siteConfig;
+  const previous = versions.slice(1);
+  const { repoUrl } = config;
 
   return (
     <div className="docMainWrapper wrapper">
@@ -30,45 +31,68 @@ function Versions(props) {
           <h3 id="latest">Current version</h3>
           <table className="versions">
             <tbody>
-              <tr>
-                <th>{latestVersion}</th>
-                <td>
-                  <a
-                    href={`${siteConfig.baseUrl}${siteConfig.docsUrl}/${
-                      props.language ? props.language + '/' : ''
-                    }getting_started`}>
-                    Documentation
-                  </a>
-                </td>
-              </tr>
+              <VersionRow
+                config={config}
+                version={latestVersion}
+                language={props.language}
+              />
             </tbody>
           </table>
+
+          {previous.length === 0 ? null :
+            <div>
+              <h3 id="previous">Previous versions</h3>
+              <table className="versions">
+                <tbody>
+                  {previous.map(v => <VersionRow key={v} config={config} version={v} language={props.language} />)}
+                </tbody>
+              </table>
+              <p>
+                You can find more past versions of this project on{' '}
+                <a href={repoUrl}>GitHub</a>.
+              </p>
+            </div>
+          }
+
           <h3 id="rc">Pre-release versions</h3>
           <table className="versions">
             <tbody>
-              <tr>
-                <th>master (next)</th>
-                <td>
-                  <a
-                    href={`${siteConfig.baseUrl}${siteConfig.docsUrl}/${
-                      props.language ? props.language + '/' : ''
-                    }next/`}>
-                    Documentation
-                  </a>
-                </td>
-                <td>
-                  <a href={siteConfig.repoUrl}>Source Code</a>
-                </td>
-              </tr>
+              <VersionRow
+                config={config}
+                version="next"
+                description="master (next)"
+                source={config.repoUrl}
+                language={props.language}
+              />
             </tbody>
           </table>
-          <p>
-            You can find past versions of this project on{' '}
-            <a href={repoUrl}>GitHub</a>.
-          </p>
         </div>
       </Container>
     </div>
+  );
+}
+
+function VersionRow(props) {
+  const { config } = props;
+  const prefix = props.version === versions[0] ? '' : props.version + '/';
+  return (
+    <tr>
+      <th>{props.description || props.version}</th>
+      <td>
+        <a
+          href={`${config.baseUrl}${config.docsUrl}/${
+            props.language ? props.language + '/' : ''
+          }${prefix}getting_started`}>
+          Documentation
+        </a>
+      </td>
+      {props.source ?
+        <td>
+          <a href={props.source}>Source Code</a>
+        </td>
+        : null
+      }
+    </tr>
   );
 }
 
