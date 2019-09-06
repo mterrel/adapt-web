5f53755bf259daf75eb90e52bc6ef23c9ee5b53f
Mark Terrel <mark@unbounded.systems>
2019-09-06 14:19:57 -0600

    Add LogoShowcase component

---
+++ adapt-web-components/src/LogoShowcase.tsx
@@ -0,0 +1,66 @@
+// tslint:disable variable-name
+
+import classNames from "classnames";
+import React from "react";
+// tslint:disable-next-line: no-var-requires
+const Safe = require("react-safe").default;
+
+const slickInit = `
+  $(document).ready(function(){
+    $('.logo-showcase').slick({
+      autoplay: true,
+      accessibility: false,
+      arrows: false,
+      draggable: false,
+      pauseOnFocus: false,
+      pauseOnHover: false,
+      slidesToShow: 3,
+      speed: 4000,
+      touchMove: false,
+      autoplaySpeed: 0,
+      cssEase: 'linear',
+      respondTo: 'slider',
+      responsive: [
+        {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 2,
+          }
+        }
+      ]
+    });
+  });
+`;
+
+export interface Logo {
+    alt: string;
+    file: string;
+    soon?: boolean;
+}
+
+export interface LogoShowcaseProps {
+    logos: Logo[];
+    pathPrefix: string;
+}
+
+export const LogoShowcase = (props: LogoShowcaseProps) => {
+    let prefix = props.pathPrefix;
+    if (prefix) prefix += "/";
+
+    return (
+        <div className="logo-showcase-wrapper">
+            <div className="logo-showcase center-block">
+                {props.logos.map((logo) =>
+                    <div className={classNames("logo-slide", { comingSoon: logo.soon })} >
+                        <img src={prefix + logo.file} />
+                    </div>
+                )}
+            </div>
+            <Safe.script type="text/javascript">{slickInit}</Safe.script>
+        </div>
+    );
+};
+
+LogoShowcase.defaultProps = {
+    pathPrefix: "",
+};
