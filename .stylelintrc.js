e2f20c814700a7f1176aa21466fb86b6b779bbcb
Mark Terrel <mark@unbounded.systems>
2019-09-11 23:50:44 -0600

    Add browser feature CSS linting - Add plugin - Add specific browserlist targets - Fix, provide fallbacks, and ignore to remove lint errors

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,5 +2,9 @@ module.exports = {
     extends: "stylelint-config-standard",
     rules: {
         indentation: 4,
-    }
+        "plugin/no-unsupported-browser-features": true,
+    },
+    plugins: [
+        "stylelint-no-unsupported-browser-features"
+    ],
 };
